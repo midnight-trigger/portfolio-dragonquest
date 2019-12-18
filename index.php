0e86bd7c94c57a60a7ed8a17b22f25f03c1433c7
Junya Nishiwaki <junya4318629@yahoo.co.jp>
2019-12-18 22:04:50 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_errors', 1);
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Dragon Quest</title>
+  <link rel="stylesheet" href="style.min.css">
+</head>
+<body>
+<div class="container">
+  <h1>Dragon Quest</h1>
+  <div class="monster">
+    <img src="img/monster01.png">
+  </div>
+  <div class="information">
+    <div class="history">
+
+    </div>
+    <div class="option">
+
+    </div>
+  </div>
+</div>
+</body>
+</html>
